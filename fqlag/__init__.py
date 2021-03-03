001350e5e71369d956d23926e34603203a1178cc
abdu Zoghbi <abzoghbi@umich.edu>
2021-03-03 16:48:48 -0500

    corrected import errors of old classes

--- fqlag/__init__.py
+++ fqlag/__init__.py
@@ -1,11 +1,11 @@
 
 from .psd import Psd
 from .psdf import Psdf
-from .cxd import Cxd, lCxd, CxdRI, lPsi, Psi
-from .pcxd import PCxd, lPCxd, PPsi, lPPsi
+from .cxd import Cxd, CxdRI, Psi
+from .pcxd import PCxd, lPCxd, PPsi
 from .cxdf import Psif, PPsif
 from . import misc
 from .multi import multiFqLagBin
 
-__all__ = ['Psd', 'Cxd', 'lPsd', 'lCxd', 'CxdRI', 'PCxd', 'lPCxd', 'PPsi', 'lPPsi', 
-            'Psi', 'lPsi', 'Psdf', 'multiFqLagBin']
+__all__ = ['Psd', 'Cxd', 'lCxd', 'CxdRI', 'PCxd', 'PPsi', 
+            'Psi', 'Psdf', 'multiFqLagBin']
