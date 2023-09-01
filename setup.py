9e332c706db1c19b4714110dc2c842929e9bb735
Abdu Zoghbi <a.zoghbi@nasa.gov>
2023-09-01 11:22:36 -0400

    set version to 0.3.4 for release

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'fqlag', 
   packages = ['fqlag'],
-  version = '0.3.1',
+  version = '0.3.4',
   license='MIT',
   description = ('Calculating Periodogram and Time delays in the frequency domain '
                 'from unevenly-sample time series'),
   author = 'Abdu Zoghbi',
   author_email = 'astrozoghbi@gmail.com',
   url = 'https://github.com/zoghbi-a/fqlag',
-  download_url = 'https://github.com/zoghbi-a/fqlag/archive/v0.3.1.tar.gz',
+  download_url = 'https://github.com/zoghbi-a/fqlag/archive/v0.3.4.tar.gz',
   keywords = ['Astronomy', 'Time-Series', 'Delays'],
   install_requires=[ 
           'scipy',
